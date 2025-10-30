680ec67d7b57e7a7be08bb2b7ba51f140307f040
Ayush Parasher <165933578+Blade-op@users.noreply.github.com>
2025-10-30 22:49:26 +0530

    yahoo finance api updated

--- backend/server.js
+++ backend/server.js
@@ -38,7 +38,7 @@ app.use('/api/comments', commentsRouter);
 
 
 // API Keys Configuration
-const YAHOO_BASE_URL = 'https://query1.finance.yahoo.com/v8/finance';
+const YAHOO_BASE_URL = 'https://query1.finance.yahoo.com/v8/finance';     /* Yahoo Finance Base URL */
 const NEWS_API_KEY = 'd200249f9c1f4721bbcb85edea0c953a';
 const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
 
